26f7475825e929d9ff69ca5e9807052c313c1507
Fillipe Eduardo <84341859+filliperubin@users.noreply.github.com>
2021-06-07 14:57:29 -0300

    Update ProdutoTableModel.java

--- src/main/java/Model/ProdutoTableModel.java
+++ src/main/java/Model/ProdutoTableModel.java
@@ -12,26 +12,73 @@ import javax.swing.table.AbstractTableModel;
 
 /**
  *
- * @author Fillipe
+ * @author Danilo Arantes <danilo at daniloarantes.com>
  */
 public class ProdutoTableModel extends AbstractTableModel {
-
+    
     private List<Produto> dados = new ArrayList<>();
     private String[] colunas = {"Descrição", "Quantidade", "Valor"};
     
     @Override
+    public String getColumnName(int column){
+        return colunas[column];
+    }
+
+    @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return dados.size();
     }
 
     @Override
     public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return colunas.length;
     }
 
     @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Object getValueAt(int linha, int coluna) {
+        switch (coluna) {
+            case 0:
+                return dados.get(linha).getDescricao();
+            case 1:
+                return dados.get(linha).getQuantidade();
+            case 2:
+                return dados.get(linha).getValor();
+        }
+        return null;
     }
+    
+    @Override
+    public void setValueAt(Object valor, int linha, int coluna){
+        switch (coluna) {
+            case 0:
+                dados.get(linha).setDescricao((String) valor);
+                break;
+            case 1:
+                dados.get(linha).setQuantidade(Integer.parseInt((String) valor));
+                break;
+            case 2:
+                dados.get(linha).setValor(Double.parseDouble((String) valor));
+                break;            
+        }
+        this.fireTableRowsUpdated(linha, linha);
+    }
+    
+    // Método para adicionar linhas na tabela
+    public void addLinha(Produto p){
+        this.dados.add(p);
+        this.fireTableDataChanged();        
+    }
+    
+    // Método para remover linha da tabela
+    public void removeLinha(int linha){
+        this.dados.remove(linha);
+        this.fireTableRowsDeleted(linha, linha);
+    }
+    
+    public Produto pegaDadosLinha(int linha){
+        return dados.get(linha);
+    }
+    
+}
     
 }
